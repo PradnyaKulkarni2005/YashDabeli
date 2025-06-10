0d541ad9cdcb2980a7bb6007525b58ecf73f633a
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2025-06-10 09:22:09 +0530

    added gallery tab used react bits circular gallery for animation

--- cafe/src/pages/Gallery.jsx
+++ cafe/src/pages/Gallery.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import CircularGallery from '../Components/CircularGallery.jsx'
 export default function Gallery() {
   return (
     <div>
+      <div style={{ height: '600px', position: 'relative' }}>
+  <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
+</div>
       
     </div>
   )
